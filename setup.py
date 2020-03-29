d87c97a89a51396550e5682d0ff9cf1f19f8a578
nucklehead <pierevans@gmail.com>
2020-03-28 23:09:41 -0400

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,22 @@ from setuptools import setup, find_packages
 setup(name='flask_restplus_data',
       version='0.0.1',
       description='Library inspired by Spring Data to perform Operations on datastores',
-      url='https://github.com/nucklehead/python-data',
-      download_url= 'https://github.com/nucklehead/python-data/tarball/1.0.0',
+      license='MIT',
+      url='https://github.com/nucklehead/flask-restplus-data',
+      download_url= 'https://github.com/nucklehead/flask-restplus-data/tarball/0.0.1',
       author='nucklehead',
       author_email='pierevans@gmail.com',
       keywords=[
         'datastore', 'sql', 'nosql', 'postgres', 'mysql', 'sqlite', 'obdc', 'oracle database', 'mongodb', 'orm', 'flask'
           , 'flask_restplus', 'rest', 'restplus'
       ],
-      classifiers=[],
+      classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Database',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3.6+',
+      ],
       packages=find_packages(exclude=['test']),
       install_requires=[
         'confuse==1.0.0',
